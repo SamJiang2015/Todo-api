52ea0045c6db64932023af476d261525dc9cd1a7
Sam Jiang <shanzi@gmail.com>
2015-11-24 19:22:07 -0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [
-	{
-		id: 1,
-		description: 'Meet mom for lunch',
-		completed: false
-	},
-	{
-		id: 2,
-		description: 'Go to market',
-		completed: false
-	},
-	{
-		id: 3,
-		description: 'Jog 10 miles',
-		completed: true
-	}	
-];
+var todos = [];
+var todoNextId = 1;  // temp way to have a unique ID
+
+app.use(bodyParser.json()); // whenever a JSON request comes in, bodyParser middleware will parse it.
 
 app.get('/',
 	function(req, res) {
 		res.send('Todo API Root');
 	});
 
+// GET /todos
 app.get('/todos',
 	function(req, res) {
 		res.json(todos);  // will convert todos object into JSON and send
 	});
 
+// GET /todos/:id
 app.get('/todos/:id', 
 	function(req, res) {
 		var todoId = parseInt(req.params.id, 10); 
@@ -47,6 +38,18 @@ app.get('/todos/:id',
 			res.status(404).send();
 		}
 	});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var newTodo = req.body;
+
+	newTodo.id = todoNextId++;
+
+	todos.push(newTodo);
+
+	res.json(newTodo);
+});
+
 
 app.listen(PORT, function() {
 	console.log('Server started on port ' + PORT);
