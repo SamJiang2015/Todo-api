d6040403a20e71ba5cc70e78af05ffb55ee1a93c
Sam Jiang <shanzi@gmail.com>
2015-11-26 13:01:05 -0800

    Add Postgres support

--- db.js
+++ db.js
@@ -1,17 +1,29 @@
 // load modules into sequelize and return the db connection to server.js
 
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development'; // Heroku sets this env varisable to 'production'
+var sequelize;
 
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-to-api.sqlite'
-});
+if (env === 'production') {
+	// we are running on Heroku
+	sequelize = new Sequelize(
+		process.env.DATABASE_URL, // this is set by Heroku, where the db is installed when we install the Heroku addon
+		{
+			dialect: 'postgres'
+		}
+	);
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-to-api.sqlite'
+	})
+}
 
 // so that we can export multiple things
 var db = {};
 
-db.todo = sequelize.import(__dirname+'/models/todo.js');
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db; 
+module.exports = db;
